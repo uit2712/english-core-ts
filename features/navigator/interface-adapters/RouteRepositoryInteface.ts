0bc530f23e030991d47a54f5572ad00b9210fb16
uit2712 <phamquangvi456@gmail.com>
2024-09-10 13:32:46 +0000

    update RouteRepositoryInteface: add setRoute

--- features/navigator/interface-adapters/RouteRepositoryInteface.ts
+++ features/navigator/interface-adapters/RouteRepositoryInteface.ts
@@ -1,5 +1,6 @@
 export interface RouteRepositoryInteface<T> {
     setRoute(route: T): void;
+    setParams(params?: any): void;
     getPathParamAsString(name: string, defaultValue?: string): string;
     getPathParamAsNumber(name: string, defaultValue?: number): number;
     getQueryParamAsString(name: string, defaultValue?: string): string;
